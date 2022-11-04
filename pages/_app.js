ce059d6825e253415732517144af0e867167219d
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-11-04 16:06:54 +0200

    modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import { TodoProvider } from "../context/TodoContext";
+import Head from "next/head";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/index.scss";
-
 function MyApp({ Component, pageProps }) {
   return (
-    <TodoProvider>
-      <Component {...pageProps} />
-      <ToastContainer position="bottom-right" autoClose={3000} theme="dark" />
-    </TodoProvider>
+    <>
+      <Head>
+        <title>Todo App</title>
+      </Head>
+      <TodoProvider>
+        <Component {...pageProps} />
+        <ToastContainer position="bottom-right" autoClose={3000} theme="dark" />
+      </TodoProvider>
+    </>
   );
 }
 
